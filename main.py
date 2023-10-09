a71ecf2778485ae3f5b8b282b0c4f31eea84b214
Dheeraj <dlovej009@gmail.com>
2023-10-09 19:32:29 +0530

    added locak db

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from flask import Flask, render_template, request, jsonify,redirect,session,url_
 import json
 from pymongo import MongoClient
 # from flask_pymongo import PyMongo
-
+# from all_users import users
 app = Flask(__name__, static_folder='static')
 # app.config["MONGO_URI"] = "mongodb+srv://dlovej009:Dheeraj2006@cluster0.dnu8vna.mongodb.net/?retryWrites=true&w=majority/myDb"
-url="mongodb+srv://dlovej009:Dheeraj2006@cluster0.dnu8vna.mongodb.net/?retryWrites=true&w=majority"
+# url="mongodb+srv://dlovej009:Dheeraj2006@cluster0.dnu8vna.mongodb.net/?retryWrites=true&w=majority"
 
-cluster=MongoClient(url)
-db=cluster['myDb']
-collection=db['users']
+# cluster=MongoClient(url)
+# db=cluster['myDb']
+# collection=db['users']
 app.secret_key = 'smart_card'
 # mongo = PyMongo(app)
 # from all_users import users
-users = list(collection.users.find({}))
-all_users = users
-print(all_users)
+# users = list(collection.users.find({}))
+
+# all_users = users
+# print(all_users)
 
 @app.route('/')
 def home():
@@ -24,9 +25,11 @@ def home():
 
 @app.route('/<user_id>')
 def index(user_id):
-    user = None
-    users = collection.users.find({"UserID": user_id})
-    user_list = list(users)
+    from all_users import users
+    user_list  =[]
+    for i in users:
+        if i["UserID"]==user_id:
+            user_list.append(i)
     if user_list:
       if user_list[0]['Authentication']['Status_of_data'] == True:
             user_list[0].pop("_id",None)
@@ -44,9 +47,9 @@ def index(user_id):
         
        
     else:
-        return "404 not valid card"
+        # return "404 not valid card"
         # # No users found for the given email
-        # return render_template('sign_up_form.html')
+        return render_template('sign_up_form.html')
     
     
 @app.route("/logout", methods=['GET', 'POST'])  
@@ -57,17 +60,23 @@ def logout():
     return redirect(f"/{uid}")
 @app.route("/login", methods=['GET', 'POST'])  
 def login():
+    from all_users import users
+   
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        users = list(collection.users.find({"Authentication.Username": username}))
-        print(users)
-        if username in users[0]['Authentication']['Username'] and users[0]['Authentication']['Password'] == password:
-            users[0].pop("_id",None)
+        userS  =[]
+        for i in users:
+            if i["Authentication"]["Username"]==username:
+                userS.append(i)
+        # users = list(collection.users.find({"Authentication.Username": username}))
+        print(userS)
+        if username in userS[0]['Authentication']['Username'] and userS[0]['Authentication']['Password'] == password:
+            userS[0].pop("_id",None)
             session['username'] = username
-            session['user']=users[0]
+            session['user']=userS[0]
             session['login']=True
-            return redirect(f'/{users[0]["UserID"]}')
+            return redirect(f'/{userS[0]["UserID"]}')
         else:
             return  render_template('login.html', error="error")
     else:
@@ -75,6 +84,7 @@ def login():
    
 @app.route("/update", methods=['GET', 'POST']) 
 def update_user():
+    from all_users import users
     if request.method == 'POST':
         print(request.referrer)
         profile_image = request.files['ProfileImage']
@@ -154,105 +164,122 @@ def update_user():
         update_operation = {
         "$set": userRecord
     }
-        filter_criteria = {"UserID": uid}
-        collection.users.update_one(filter_criteria, update_operation)
+        user =None
+        index=0
+        for i in users:
+            if i["UserID"]==uid:
+                user=i
+                break
+            index+=1 
+        users[index]= userRecord 
+        f=open("all_users.py",'w',encoding='utf-8')
+        f.write("users = "+str(users))
+        f.close() 
+        # # # # # # set user = userdetails
+        # filter_criteria = {"UserID": uid}
+        # collection.users.update_one(filter_criteria, update_operation)
         return  redirect(f"/{uid}")
     else:
         print(request.referrer)
         uid=request.referrer.split("/")[-1]
-        users = collection.users.find({"UserID": uid})
-        user=(list(users))[0]
+        user_list  =[]
+        for i in users:
+            if i["UserID"]==uid:
+                user_list.append(i)
+        # users = collection.users.find({"UserID": uid})
+        user=user_list[0]
         return render_template('update.html',user=user)
         
     
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-  if request.method == 'POST':
-    print(request.referrer)
-    profile_image = request.files['ProfileImage']
-    cover_image = request.files['CoverImage']
-    try:
-        # Save the uploaded files to a desired location
-        profile_image.save(f'static/{profile_image.filename}')
-        cover_image.save(f'static/{cover_image.filename}')
-    except:
-        pass
-    
-    uid=request.referrer.split("/")[-1]
-    print('this is post request')
-    userRecord={
-        "UserID":uid,
-        "FirstName":request.form.get("FName"),
-        "LastName": request.form.get("LName"),
-        "ProfileImage":f"static/{profile_image.filename}",
-        "CoverImage":f"static/{cover_image.filename}",
-        "Contact": request.form.get("Contact"),
-        "DOB":request.form.get("DOB"),
-        "BusinessName": request.form.get("BusinessName"),
-        "Designation":request.form.get("Designation"),
-        "Website":request.form.get("Website"),
-        "Socials": {
-            "Twitter": request.form.get("Twitter"),
-            "Instagram":request.form.get("Instagram"),
-            "LinkedIn":request.form.get("LinkedIn"),
-            "Facebook":request.form.get("Facebook"),
-            "YouTube":request.form.get("YouTube"),
-            "GoogleMapUrl":request.form.get("GoogleMapUrl"),
-            "SnapChat":request.form.get("SnapChat"),
-            "DriveLink":request.form.get("DriveLink")
-        },
-        "About":{
-            "GSTIN":"",
-            "PAN Number":"",
-            "AboutText":request.form.get("aboutText")
-        },
-        "Location": {
-            "Address": "123 Main Street, Cityville",
-            "ZipCode":"",
-            "Latitude": 134.563252,
-            "Longitude": 185.5342678,
-            "MapUrl": "https://map.google.com/xyz…"
-        },
-
-        "Gallery": {
-            "IMG1":"",
-            "IMG2":"",
-            "IMG3":"",
-            "IMG4":"",
-            "IMG5":"",
-        },
-        "Payment": {
-            "QRImage":"QRImage.jpg",
-            "PayPal": "johndoe_paypal@example.com",
-            "CreditCard": "**** **** **** 1234",
-            "BankName":"",
-            "AccountHolderName":"",
-            "AccountNumber":"",
-            "IFSC":"",
-            "GooglePay":"",
-            "PhonePe":"",
-            "Paytm":"",
-            "UPI":"",
-        },
+    from all_users import users
+    if request.method == 'POST':
+        print(request.referrer)
+        profile_image = request.files['ProfileImage']
+        cover_image = request.files['CoverImage']
+        try:
+            # Save the uploaded files to a desired location
+            profile_image.save(f'static/{profile_image.filename}')
+            cover_image.save(f'static/{cover_image.filename}')
+        except:
+            pass
         
-         
-        "Authentication": {
-            "Username": request.form.get("Email"),
-            "Password": request.form.get("Password"),
-            "Status_of_data": True,
-        }
-    }
-    # users.append(userRecord)
+        uid=request.referrer.split("/")[-1]
+        print('this is post request')
+        userRecord={
+            "UserID":uid,
+            "FirstName":request.form.get("FName"),
+            "LastName": request.form.get("LName"),
+            "ProfileImage":f"static/{profile_image.filename}",
+            "CoverImage":f"static/{cover_image.filename}",
+            "Contact": request.form.get("Contact"),
+            "DOB":request.form.get("DOB"),
+            "BusinessName": request.form.get("BusinessName"),
+            "Designation":request.form.get("Designation"),
+            "Website":request.form.get("Website"),
+            "Socials": {
+                "Twitter": request.form.get("Twitter"),
+                "Instagram":request.form.get("Instagram"),
+                "LinkedIn":request.form.get("LinkedIn"),
+                "Facebook":request.form.get("Facebook"),
+                "YouTube":request.form.get("YouTube"),
+                "GoogleMapUrl":request.form.get("GoogleMapUrl"),
+                "SnapChat":request.form.get("SnapChat"),
+                "DriveLink":request.form.get("DriveLink")
+            },
+            "About":{
+                "GSTIN":"",
+                "PAN Number":"",
+                "AboutText":request.form.get("aboutText")
+            },
+            "Location": {
+                "Address": "123 Main Street, Cityville",
+                "ZipCode":"",
+                "Latitude": 134.563252,
+                "Longitude": 185.5342678,
+                "MapUrl": "https://map.google.com/xyz…"
+            },
 
-    collection.users.insert_one(userRecord)
-    f=open("all_users.py",'w',encoding='utf-8')
-    f.write("users = "+str(users))
-    f.close()
-    return  redirect(f"/{uid}")
+            "Gallery": {
+                "IMG1":"",
+                "IMG2":"",
+                "IMG3":"",
+                "IMG4":"",
+                "IMG5":"",
+            },
+            "Payment": {
+                "QRImage":"QRImage.jpg",
+                "PayPal": "johndoe_paypal@example.com",
+                "CreditCard": "**** **** **** 1234",
+                "BankName":"",
+                "AccountHolderName":"",
+                "AccountNumber":"",
+                "IFSC":"",
+                "GooglePay":"",
+                "PhonePe":"",
+                "Paytm":"",
+                "UPI":"",
+            },
+            
+            
+            "Authentication": {
+                "Username": request.form.get("Email"),
+                "Password": request.form.get("Password"),
+                "Status_of_data": True,
+            }
+        }
+        # users.append(userRecord)
+        users.append(userRecord)
+        # collection.users.insert_one(userRecord)
+        f=open("all_users.py",'w',encoding='utf-8')
+        f.write("users = "+str(users))
+        f.close()
+        return  redirect(f"/{uid}")
+        
     
-  
-  else:
-    return "non post"
+    else:
+        return "non post"
   
 
 app.run(debug=True)
