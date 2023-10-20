081aa425cbc07466079051d05993021b0b0a66b7
Dheeraj <dlovej009@gmail.com>
2023-10-20 22:11:15 +0530

    hosting process

--- main.py
+++ main.py
@@ -376,7 +376,7 @@ def contact():
   
 # port = int(os.environ.get('PORT', 5000))
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
 
