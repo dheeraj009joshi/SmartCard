bf9a49355057172051d27c5bd97cf105a9411d8c
Dheeraj <dlovej009@gmail.com>
2023-10-20 21:39:39 +0530

    hosting process

--- main.py
+++ main.py
@@ -374,9 +374,9 @@ def contact():
     return render_template("contact.html")
 
   
-port = int(os.environ.get('PORT', 5000))
+# port = int(os.environ.get('PORT', 5000))
 
-
-app.run(debug=True, host='0.0.0.0',port=port)
+if __name__ == "__main__":
+    app.run()
 
 
