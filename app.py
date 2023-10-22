6092f4ae8f7170ed9caf3789a3e1f1d6ff13a00a
Dheeraj <dlovej009@gmail.com>
2023-10-22 06:41:16 +0530

    dj

--- app.py
+++ app.py
@@ -374,9 +374,9 @@ def contact():
     return render_template("contact.html")
 
   
-# port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
 
 
