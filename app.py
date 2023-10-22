a02ac217ae01a1b0155045f101529a2d69777460
Dheeraj <dlovej009@gmail.com>
2023-10-22 07:28:15 +0530

    gj

--- app.py
+++ app.py
@@ -377,6 +377,6 @@ def contact():
 port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True,host='0.0.0.0', port=port)
 
 
